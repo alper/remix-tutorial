a8ffca3ca5e8c74ab38c703c27f00f8e0e6669af
Alper Cugun <github@alper.nl>
2024-06-21 22:20:27 +0200

    REdirect new records to edit page

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
   Link,
@@ -9,7 +9,7 @@ import {
   ScrollRestoration,
   useLoaderData,
 } from "@remix-run/react";
-import type { LinksFunction, json } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 
 import appStylesHref from "./app.css?url";
 
@@ -22,7 +22,7 @@ export const loader = async () => {
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export const links: LinksFunction = () => {
